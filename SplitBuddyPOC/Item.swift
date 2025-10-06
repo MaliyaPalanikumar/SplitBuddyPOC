150359ee0905a3a5f60ecd5df2ba7deac86f83e0
Maliya <maliyapalanikumar@gmail.com>
2025-10-06 11:45:26 +0530

    Initial Commit

---
+++ SplitBuddyPOC/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SplitBuddyPOC
+//
+//  Created by Maliya Palanikumar on 06/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
